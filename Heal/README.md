31480af4d0a1ae91b251b220bd1e0ccfc4a7bca9
Karanpatel567 <karan.varsani123@gmail.com>
2017-09-08 02:09:24 -0500

    Add files via upload

---
+++ Heal/README.md
@@ -0,0 +1,3 @@
+This plugin is made by Karanpatel567, this plugin has the ability to heal people from console and can heal
+your self via the command "/heal" and others via the command "/heal {player name}". Example you want to heal
+someone else i would do "/heal test"
---
+++ Heal/src/Heal/main.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Heal;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as c;
+use pocketmine\Player;
+
+/* Copyright (C) Karanpatel567 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Karanpatel567 <karanpatel1908@outlook.com>, September 2017
+ */
+
+
+class main extends PluginBase{
+     public function onEnable(){
+          $this->getLogger()->info(c::GREEN. "Heal by Karan enabled!");
+     }
+     public function onCommand(CommandSender $sender, Command $command, $labels, array $args){
+          $cmd = strtolower($command);
+          if($cmd == "heal"){  //registers the command
+               if($sender->hasPermission("heal.command") && $sender instanceof Player) {
+                    $sender->setHealth($sender->getMaxHealth());
+                    $sender->sendMessage(c::YELLOW."You've been healed!");
+               }
+               if(isset($args[0])){
+                    if($sender->hasPermission("heal.other")){
+                      $player = $this->getServer()->getPlayer($args[0]);
+                      if($player !== null){
+                          $player->setHealth($sender->getMaxHealth());
+                          $sender->sendMessage(c::YELLOW. "$args[0] has been healed");
+                          $player->sendMessage(c::YELLOW. "You have been healed by ". $sender->getName()); //sends messages when healed
+                     }else{
+                          $sender->sendMessage(c::RED. "Oops, player is not online!");
+                     }
+                    }
+               }
+          }
+     }
+     public function onDisable(){
+          $this->getLogger()->info(c::RED. "Heal by Karan disabled!");
+     }
+}
